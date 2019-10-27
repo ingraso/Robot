33614443265ccc831898704e9a11ec38b2dc098c
Ingrid Amalie <iasolbjorg@gmail.com>
2019-10-27 14:40:56 +0100

    Change from behaviours to behaviors

--- bbcon.py
+++ bbcon.py
@@ -13,9 +13,9 @@ class Bbcon:
     """BBCON klass"""
 
     time_step = 0.4  # må velge varighet på time_step for robot
-    behaviour_objects = []  # alle behaviour_objects som finnes
+    behavior_objects = []  # alle behaviour_objects som finnes
     sensobs_objects = []  # alle sensobs objekter som robot har
-    active_behaviours = []  # behaviours som LAGER anbefalinger basert på input fra sensobs
+    active_behaviors = []  # behaviours som LAGER anbefalinger basert på input fra sensobs
     motobs_objects = []  ## alle motobs objecter som snakker med hjulene
     total_time = 0
 
@@ -24,24 +24,24 @@ class Bbcon:
         self.arbitrator = arbitrator.Arbitrator(self)  # Arbitrator skal ta imot selve Bbcon()
         self.motobs_objects = motobs_objects  # alle motobs objecter som snakker med hjulene
 
-    def add_behaviour(self, behaviour):
+    def add_behavior(self, behavior):
         """Funksjon for å legge ny behaviours til behaviour_objects list"""
-        self.behaviour_objects.append(behaviour)
+        self.behavior_objects.append(behavior)
 
     def add_sensor(self, sensor):
         """Funksjonen for å legge ny behaviour til sensor_objects list"""
         self.sensobs_objects.append(sensor)
 
-    def activate_behaviour(self, behaviour):
+    def activate_behavior(self, behavior):
         """Funksjonen for å legge ny behaviour til active_behaviour list"""
-        if behaviour in self.behaviour_objects:
-            self.active_behaviours.append(behaviour)
+        if behavior in self.behavior_objects:
+            self.active_behaviors.append(behavior)
         else:
             print("this behaviour is not in the list")
 
-    def deactivate_behaviour(self, behaviour):
+    def deactivate_behavior(self, behavior):
         """Funksjonen for å fjerne behaviour fra active_behaviour list """
-        self.active_behaviours.remove(behaviour)
+        self.active_behaviors.remove(behavior)
 
     def run_one_timestep(self):
         """Kjøre en runde med viss timestap"""
@@ -50,21 +50,21 @@ class Bbcon:
             sens.update()
             print("Sensob :" + str(sens) + "har blitt oppdatert")
 
-        for behav in self.behaviour_objects:
+        for behav in self.behavior_objects:
             # oppdaterer alle behaviour, s.a. de kan lese fra sensobs of sette
             # seg selv som aktive eller ikke
             behav.update()
-            print("Behaviour_object :" + str(behav) + "har blitt oppdatert")
+            print("Behavior_object :" + str(behav) + "har blitt oppdatert")
 
-        for behav in self.behaviour_objects:  # finner ut om behaviour er aktiv eller ikke
+        for behav in self.behavior_objects:  # finner ut om behaviour er aktiv eller ikke
             if behav.active_flag:
-                self.activate_behaviour(behav)
-                print("Behaviour_object :" + str(behav) + "aktivert")
+                self.activate_behavior(behav)
+                print("Behavior_object :" + str(behav) + "aktivert")
             else:
-                if behav in self.active_behaviours:
-                    self.deactivate_behaviour(behav)
-                    print("Behaviour_object :" + str(behav) + "deaktivert")
-        action = self.arbitrator.choose_action()  #kaller arbitrator for å velge en aksjon
+                if behav in self.active_behaviors:
+                    self.deactivate_behavior(behav)
+                    print("Behavior_object :" + str(behav) + "deaktivert")
+        action = self.arbitrator.choose_action()  # kaller arbitrator for å velge en aksjon
         print("arbitrator valgte :" + str(action))
 
         for mot in self.motobs_objects:
@@ -82,12 +82,12 @@ class Bbcon:
 if __name__ == "__main__":
     print("*****Hello there, it's I, THE BBCON!!!*****")
     BBCON = Bbcon(motob.motob())  # skrive inn motobs
-    BBCON.add_behaviour(behaviours.Behavior1(sensob.LineDetector(), BBCON))  # legg til alle behaviours
-    BBCON.add_behaviour(behaviours.Behavior2(sensob.MeasureDistance(), BBCON))
-    BBCON.add_behaviour(behaviours.Behavior3(sensob.MeasureDistance(), sensob.Cameraob(), sensob.LineDetector(), BBCON))
-    BBCON.add_behaviour(behaviours.Behavior4(sensob.MeasureDistance(), sensob.Cameraob(), BBCON))
-    BBCON.add_behaviour(behaviours.Behavior5(sensob.MeasureDistance(), sensob.Cameraob(), BBCON))
-    BBCON.add_behaviour(behaviours.Behavior6(BBCON))
+    BBCON.add_behavior(behavior.Behavior1(sensob.LineDetector(), BBCON))  # legg til alle behaviours
+    BBCON.add_behavior(behavior.Behavior2(sensob.MeasureDistance(), BBCON))
+    BBCON.add_behavior(behavior.Behavior3(sensob.MeasureDistance(), sensob.Cameraob(), sensob.LineDetector(), BBCON))
+    BBCON.add_behavior(behavior.Behavior4(sensob.MeasureDistance(), sensob.Cameraob(), BBCON))
+    BBCON.add_behavior(behavior.Behavior5(sensob.MeasureDistance(), sensob.Cameraob(), BBCON))
+    BBCON.add_behavior(behavior.Behavior6(BBCON))
     #BBCON.add_sensor(sensob.Proximity())  # legg til alle senobs
     BBCON.add_sensor(sensob.LineDetector())  # legg til alle senobs
     BBCON.add_sensor(sensob.MeasureDistance())  # legg til alle senobs
