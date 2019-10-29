f4886f79b455dc6d70d592ad6cd7045c17b5b369
Ingrid Amalie Solbjørg <43182025+ingraso@users.noreply.github.com>
2019-10-29 14:29:06 +0100

    Update motob.py

--- motob.py
+++ motob.py
@@ -31,10 +31,10 @@ class motob:
         #print("I am in loop")
         if self.value[0] == 'l' and self.value[1] > 0:
             #cond_left_right = True
-            self.motor.set_value((self.value[2], -self.value[2]), turn_speed #10)
+            self.motor.set_value((self.value[2], -self.value[2]), turn_speed)#10
         elif self.value[0] == 'r' and self.value[1] > 0:
             #cond_left_right = True
-            self.motor.set_value((-self.value[2], self.value[2]), turn_speed #10)
+            self.motor.set_value((-self.value[2], self.value[2]), turn_speed)#10
         elif self.value[0] == 'f' and self.value[1] == 0:
             self.motor.forward(abs(self.value[2]), dur)
         elif self.value[0] == 'b' and self.value[1] == 0:
