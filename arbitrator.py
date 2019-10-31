e9c66dd76adb81e0a0cf88c1a6e676817bae2bfb
unknown <karoline.l.langli@icloud.com>
2019-10-31 15:45:50 +0100

    fixed pylint score

--- arbitrator.py
+++ arbitrator.py
@@ -12,8 +12,9 @@ class Arbitrator:
         which must have a pointer to it's bbcon"""
         self.bbcon = bbcon
         self.default = None
-    
+
     def set_default_current_behavior(self, default):
+        """To set a default current behavior"""
         self.default = default
 
     def choose_action(self):
@@ -21,7 +22,7 @@ class Arbitrator:
         the tuple """
         active_behaviors = self.bbcon.active_behaviors
         print(active_behaviors)
-        
+
         # finner behavior med høyest weight
         highest = 0
         correct_behavior = None
@@ -32,37 +33,15 @@ class Arbitrator:
                 correct_behavior = act_b
                 highest = act_b.weight
         print()
-        
-        if correct_behavior == None:
+
+        if correct_behavior is None:
             print("_____________________KODEN SKULLE KRASJET PGA NONE______________")
             correct_behavior = self.default
-        
-        """
-        interval = []
-        current_num = 0
-        for behavior in active_behaviors:
-            # print("the weight of this behavior is:", behavior.weight)
-            # Creates a list containing all the intervals for the behaviours
-            current_num += behavior.weight
-            interval.append(round(current_num, 2))
-        print("intervallet er:", interval)
-        # Choose a random num in the full interval:
-        random_number = random.uniform(0, interval[-1])
-        print("Random number er:", random_number)
-        correct_behavior = self.default
-        for i in range(0, len(interval)):
-            # print("Nå er vi i for-løkken, runde: ", i)
-            if random_number < interval[i]:
-                print("Nå er vi i if-setningen")
-                # Find correct behaviour and end the for-loop
-                correct_behavior = active_behaviors[i]
-                break
-            else:
-                print("(arbitrator)Kom ikke in i if (arbitrator)")
-        # print("Nå er motor_recomendation", correct_behavior.motor_recommendations)
-        # print("Nå er halt_request", correct_behavior.halt_request)
-        """
+
         print("correct_behavior:", correct_behavior)
-        print("correct_behavior.motor_recommondations: {}".format(correct_behavior.motor_recommendations))
-        tupple = (correct_behavior.motor_recommendations, correct_behavior.halt_request)
+        print("correct_behavior.motor_recommondations: {}".format(
+            correct_behavior.motor_recommendations))
+        tupple = (
+            correct_behavior.motor_recommendations,
+            correct_behavior.halt_request)
         return tupple
