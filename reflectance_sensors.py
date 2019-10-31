4c69829bdc1616cbe8c14cbad60a191c661374af
karoliks <49613887+karoliks@users.noreply.github.com>
2019-10-31 13:30:40 +0100

    Update reflectance_sensors.py

--- reflectance_sensors.py
+++ reflectance_sensors.py
@@ -8,6 +8,10 @@ class ReflectanceSensors():
     # The constructor allows students to decide if they want to auto_calibrate
     # the robot, or if they want to hard code the min and max readings of the
     # reflectance sensors
+    
+    def __repr__(self):
+        return "ReflectanceSensors"
+    
     def __init__(self, auto_calibrate=False, min_reading=100, max_reading=1000):
         self.setup()
         if (auto_calibrate):
